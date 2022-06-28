7f27231192d12a51bcc0bad99e4843ab60fa773a
vini <vinnicius9511@gmail.com>
2022-06-28 19:47:40 -0300

    adicionando docker para mysql e redis

---
+++ mysql-container/db/example.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+	`name` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) NOT NULL,
+	`phone` VARCHAR(20) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
